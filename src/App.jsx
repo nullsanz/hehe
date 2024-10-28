4c3d4fe77d06b5422981ff7c209df1392902ee61
nullsanz <104490140+nullsanz@users.noreply.github.com>
2024-10-28 09:00:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
     <main>
       <Navbar />
       <audio ref={audioRef}>
-        <source src="/src/ms.mp3" type="mp3/mpeg" /> {/* Ganti path di sini */}
+        <source src="/src/ms.mp3" type="audio/mpeg" /> {/* Ganti path di sini */}
         Your browser does not support the audio element.
       </audio>
       <section
